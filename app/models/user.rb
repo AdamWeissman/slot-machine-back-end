1d68ef2de74e2681100a0d74e1eb8341e7ceb00c
AdamWeissman <adam@comedytragedyepic.com>
2020-07-17 22:09:02 -0400

    dependent destroy setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_one :bank, dependent: :destroy
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end
