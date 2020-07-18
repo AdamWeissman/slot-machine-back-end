b6222ffe23bafe4d84d7d3a1f3b85aaa9f981cf4
AdamWeissman <adam@comedytragedyepic.com>
2020-07-17 21:43:55 -0400

    minor changes to application users and auth controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,15 +5,17 @@ class Api::V1::UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
-      render json: { user: User.new(@user), jwt: @token }, status: :created
+      render json: { user: @user, jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
+
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
