dc9be0223248c41956feef03eb31e1e138668ded
AdamWeissman <adam@comedytragedyepic.com>
2020-07-17 22:14:51 -0400

    got rid of destroy route for bank since it's dependent destroy on the user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :show, :destroy]
-      resources :banks, only: [:create, :update, :show, :destroy]
+      resources :banks, only: [:create, :update, :show]
       post '/login', to: 'auth#login'
     end
   end
